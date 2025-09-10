8ef2feb6c3d70555ba379d61e74e70c6ffc735b4
shafi-vilayil2201-cyber <shafivilayil2201@gmail.com>
2025-09-10 20:04:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// const menuBtn = document.getElementById('menu-btn');
+//       const mobileMenu = document.getElementById('mobile-menu');
+//       menuBtn.addEventListener('click', function() {
+//         mobileMenu.classList.toggle('hidden');
+// //       });
+// const menuBtn = document.getElementById('menu-btn');
+// const mobileMenu = document.getElementById('mobile-menu');
+//   menuBtn.addEventListener('click', () => {
+//       mobileMenu.classList.toggle('hidden');
+// });
+
+
